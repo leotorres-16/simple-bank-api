e456eaad9c21e8b3a68a320decb5397b88531ea6
leotorres-16 <torres.leo16@gmail.com>
2025-06-08 17:14:13 +0100

    feat: finished the infra as code stack

--- packages/stack/lib/helpers/configureAPIGateway.ts
+++ packages/stack/lib/helpers/configureAPIGateway.ts
@@ -3,6 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import * as crypto from "crypto";
 import { Construct } from "constructs";
 import { Lambdas } from "./createLambdas";
+import { IFunction } from "aws-cdk-lib/aws-lambda";
 
 interface ConfigureApiGatewayProps {
   stage: string;
@@ -22,16 +23,45 @@ export const configureAPIGateway = (scope: Construct, props: ConfigureApiGateway
   });
 
   //create user endpoint (users)
-  const usersEndpoint = version.addResource("users", {
+  const userEndpoints = configureUserEndpoints(version, props.lambdas.userEndpoint);
+
+  const accountsEndpoint = version.addResource("accounts", {
     defaultCorsPreflightOptions: {
       allowOrigins: ["*"],
-      allowMethods: ["OPTIONS", "POST", "GET"],
+      allowMethods: ["OPTIONS", "POST", "GET", "PATCH", "DELETE"],
     },
   });
 
-  const getUserEndpoint = usersEndpoint.addMethod(
-    "GET",
-    new apigw.LambdaIntegration(props.lambdas.userEndpoint, {
+  const withAccountId = accountsEndpoint.addResource("{accountId}");
+
+  const accountEndpoints = configureAccountEndpoints(accountsEndpoint, withAccountId, props.lambdas.accountEndpoint);
+  const transactionsEndpoint = configureTransactionsEndpoints(withAccountId, props.lambdas.transactionEndpoint);
+
+  //deploy the new endpoints
+  const d = new Date();
+  const md5 = crypto.createHash("md5").update(d.toISOString()).digest("hex");
+  const apigwDeploy = new apigw.Deployment(scope, `${props.stage}.banking${md5.slice(0, 8)}`, { api: bankingAPI, retainDeployments: true });
+  apigwDeploy.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE);
+  [userEndpoints, accountEndpoints, transactionsEndpoint].forEach((endpoint) => {
+    endpoint.forEach((method) => {
+      apigwDeploy._addMethodDependency(method);
+    });
+  });
+  /* @ts-ignore */
+  apigwDeploy.resource.stageName = props.stage;
+};
+
+const configureUserEndpoints = (root: apigw.Resource, lambda: IFunction): apigw.Method[] => {
+  const usersEndpoint = root.addResource("users", {
+    defaultCorsPreflightOptions: {
+      allowOrigins: ["*"],
+      allowMethods: ["OPTIONS", "POST", "GET", "DELETE", "PATCH"],
+    },
+  });
+
+  const createUserEndpoint = usersEndpoint.addMethod(
+    "POST",
+    new apigw.LambdaIntegration(lambda, {
       timeout: cdk.Duration.seconds(29),
     }),
     {
@@ -39,18 +69,124 @@ export const configureAPIGateway = (scope: Construct, props: ConfigureApiGateway
     }
   );
 
-  const userEndpoints = [getUserEndpoint];
+  const withId = usersEndpoint.addResource("{userId}");
 
-  //deploy the new endpoints
-  const d = new Date();
-  const md5 = crypto.createHash("md5").update(d.toISOString()).digest("hex");
-  const apigwDeploy = new apigw.Deployment(scope, `${props.stage}.banking${md5.slice(0, 8)}`, { api: bankingAPI, retainDeployments: true });
-  apigwDeploy.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE);
-  [userEndpoints].forEach((endpoint) => {
-    endpoint.forEach((method) => {
-      apigwDeploy._addMethodDependency(method);
-    });
+  const getUserEndpoint = withId.addMethod(
+    "GET",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const deleteUserEndpoint = withId.addMethod(
+    "DELETE",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const updateUserEndpoint = withId.addMethod(
+    "PATCH",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  return [getUserEndpoint, createUserEndpoint, deleteUserEndpoint, updateUserEndpoint];
+};
+
+const configureAccountEndpoints = (root: apigw.Resource, withAccountId: apigw.Resource, lambda: IFunction): apigw.Method[] => {
+  const createAccountEndpoint = root.addMethod(
+    "POST",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const getAccountsEndpoint = root.addMethod(
+    "GET",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const deleteAccountEndpoint = withAccountId.addMethod(
+    "DELETE",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const updateAccountEndpoint = withAccountId.addMethod(
+    "PATCH",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  return [getAccountsEndpoint, createAccountEndpoint, deleteAccountEndpoint, updateAccountEndpoint];
+};
+
+const configureTransactionsEndpoints = (root: apigw.Resource, lambda: IFunction): apigw.Method[] => {
+  const TransactionsEndpoint = root.addResource("transactions", {
+    defaultCorsPreflightOptions: {
+      allowOrigins: ["*"],
+      allowMethods: ["OPTIONS", "POST", "GET"],
+    },
   });
-  /* @ts-ignore */
-  apigwDeploy.resource.stageName = props.stage;
+
+  const createTransactionEndpoint = TransactionsEndpoint.addMethod(
+    "POST",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const listTransactionsEndpoint = TransactionsEndpoint.addMethod(
+    "GET",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  const withTransactionId = TransactionsEndpoint.addResource("{transactionId}");
+
+  const fetchTransactionEndpoint = withTransactionId.addMethod(
+    "GET",
+    new apigw.LambdaIntegration(lambda, {
+      timeout: cdk.Duration.seconds(29),
+    }),
+    {
+      apiKeyRequired: true,
+    }
+  );
+
+  return [createTransactionEndpoint, listTransactionsEndpoint, fetchTransactionEndpoint];
 };
--- packages/stack/lib/helpers/createLambdas.ts
+++ packages/stack/lib/helpers/createLambdas.ts
@@ -5,6 +5,8 @@ import { SimpleLambda } from "../constructs/SimpleLambda";
 
 export interface Lambdas {
   userEndpoint: lambda.Function;
+  accountEndpoint: lambda.Function;
+  transactionEndpoint: lambda.Function;
 }
 
 interface CreateLambdasProps {
@@ -12,6 +14,8 @@ interface CreateLambdasProps {
 }
 
 const USER_ENDPOINT_NAME = "user-endpoint";
+const ACCOUNT_ENDPOINT_NAME = "account-endpoint";
+const TRANSACTION_ENDPOINT_NAME = "transaction-endpoint";
 
 export const createLambdas = (scope: Construct, props: CreateLambdasProps): Lambdas => {
   //Create the execution roles for the lambdas
@@ -19,9 +23,13 @@ export const createLambdas = (scope: Construct, props: CreateLambdasProps): Lamb
 
   //Create the lambdas
   const userEndpointLambdaFunction = new SimpleLambda(scope, USER_ENDPOINT_NAME, defaultExecutionRole, "UserEndpointLambdaLogs");
+  const accountEndpointLambdaFunction = new SimpleLambda(scope, ACCOUNT_ENDPOINT_NAME, defaultExecutionRole, "AccountEndpointLambdaLogs");
+  const transactionEndpointLambdaFunction = new SimpleLambda(scope, TRANSACTION_ENDPOINT_NAME, defaultExecutionRole, "TransactionEndpointLambdaLogs");
 
   //return the lambdas
   return {
     userEndpoint: userEndpointLambdaFunction.lambda,
+    accountEndpoint: accountEndpointLambdaFunction.lambda,
+    transactionEndpoint: transactionEndpointLambdaFunction.lambda,
   };
 };
